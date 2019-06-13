6cb76a79b92f75665953997d5d672769b8f2621e
Solal Pirelli <solal.pirelli@gmail.com>
2019-06-13 12:58:13 +0200

    Fix #7, judgehosts now init before running the judgedaemon.

--- scripts/judge.sh
+++ scripts/judge.sh
@@ -9,32 +9,15 @@
 # $SERVER_PASSWORD: the password of the 'judgehost' user on the server
 # $LANGUAGES: the languages to install in the judge
 
-# HACK wait until Docker has finished initializing fully, otherwise the client can't find the socket
-sleep 20
 
-# Delete the container first, just in case
-sudo docker rm -f judge >> /dev/null 2>&1 || true
-# Note that --hostname is what DOMjudge will report as the judge name
-sudo docker run --name=judge \
-                --hostname="$NAME" \
-                --detach \
-                --restart=always \
-                --privileged \
-                --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro \
-                -e "CONTAINER_TIMEZONE=$TIMEZONE" \
-                -e "DOMSERVER_BASEURL=$SERVER/" \
-                -e 'JUDGEDAEMON_USERNAME=judgehost' \
-                -e "JUDGEDAEMON_PASSWORD=$SERVER_PASSWORD" \
-                -e 'DAEMON_ID=0' \
-                domjudge/judgehost:5.3.3
-
-
-# HACK wait until the judge has initialized (how do we do that properly?)
-sleep 20
-
-# Write down the command we'll execute within Docker.
+# Write down the init script.
 # Note that the parameters are not expanded (thanks to the quoted EOF), but given later
-cat > command.sh << 'EOF'
+cat > 'judge-init.sh' << 'EOF'
+# Don't re-execute the script if it finished once
+if [ -f '/.judge_init_done' ]; then
+  exit 0
+fi
+
 # Mount stuff so that packages can be installed properly
 mount -t proc proc '/chroot/domjudge/proc'
 mount -t sysfs sysfs '/chroot/domjudge/sys'
@@ -78,6 +61,7 @@ for lang in $LANGUAGES; do
       ;;
     java11)
       install='-t testing openjdk-11-jdk'
+      outside='true'
       ;;
     python27)
       install='python2.7'
@@ -111,10 +95,36 @@ done
 umount '/chroot/domjudge/proc'
 umount '/chroot/domjudge/sys'
 umount '/chroot/domjudge/dev/pts'
+
+# Don't re-execute the script if it finished once
+touch '/.judge_init_done'
 EOF
 
-# Execute the command
-sudo docker exec judge bash -c "LANGUAGES='$LANGUAGES'; $(cat command.sh)"
+# Make it executable
+chmod +x 'judge-init.sh'
 
-# Remove the command file, useless now
-rm command.sh
+
+# HACK wait until Docker has finished initializing fully, otherwise the client can't find the socket
+sleep 20
+
+# Delete the container first, just in case
+sudo docker rm -f judge >> /dev/null 2>&1 || true
+# Note that --hostname is what DOMjudge will report as the judge name
+# We use --volume to share the judge-init.sh script, and --entrypoint to execute it before the normal entrypoint
+# We need the normal init to set stuff up, but before launching the judgedaemon, we want our init.
+# Note that parameters to the --entrypoint are passed after the image name.
+sudo docker run --name=judge \
+                --hostname="$NAME" \
+                --detach \
+                --restart=always \
+                --privileged \
+                --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro \
+                "--volume=$(pwd)/judge-init.sh:/judge-init.sh:ro" \
+                --entrypoint /bin/sh \
+                -e "CONTAINER_TIMEZONE=$TIMEZONE" \
+                -e "DOMSERVER_BASEURL=$SERVER/" \
+                -e 'JUDGEDAEMON_USERNAME=judgehost' \
+                -e "JUDGEDAEMON_PASSWORD=$SERVER_PASSWORD" \
+                -e 'DAEMON_ID=0' \
+                domjudge/judgehost:5.3.3 \
+                -c "sed -i '/judgedaemon/i /judge-init.sh' /scripts/start.sh; export LANGUAGES='$LANGUAGES'; /scripts/start.sh"
